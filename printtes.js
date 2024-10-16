472acf79eb86b738d871f521e89ca87f4f3b157f
Fakhry Hizballah <50532299+fakhryhizballah@users.noreply.github.com>
2024-10-16 16:59:19 +0700

    Update printtes.js

--- printtes.js
+++ printtes.js
@@ -2,13 +2,15 @@ const escpos = require('escpos');
 // install escpos-usb adapter module manually
 escpos.USB = require('escpos-usb');
 // Select the adapter based on your printer type
+
+  const devices = escpos.USB.findPrinter();
+  console.log(devices)
 const device  = new escpos.USB();
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
 const options = { encoding: "GB18030" /* default */ }
 // encoding is optional
-
 const printer = new escpos.Printer(device, options);
 
 device.open(function(error){
