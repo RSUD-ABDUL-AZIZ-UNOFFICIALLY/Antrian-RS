8cea6546c7eac9fa6faa54c0ddd4e7716093a485
fakhryhizballah <falehry88@gmail.com>
2024-10-16 11:37:17 +0700

    restore

--- models/index.js
+++ models/index.js
@@ -11,11 +11,11 @@ console.log(config);
 const db = {};
 
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+}
 
 fs
   .readdirSync(__dirname)
