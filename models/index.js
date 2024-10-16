44fe5a6e7eeb878e7e3ed3ff2f56141186e31540
fakhryhizballah <falehry88@gmail.com>
2024-10-16 11:13:06 +0700

    test confg

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
+console.log(config);
 const db = {};
 
 let sequelize;
