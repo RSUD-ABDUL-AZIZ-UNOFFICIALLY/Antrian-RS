bc274f329b66088a37991564408d12b79c950f4d
fakhryhizballah <falehry88@gmail.com>
2024-11-02 23:39:52 +0700

    add pool

--- models/index.js
+++ models/index.js
@@ -13,7 +13,14 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, {
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
 }
 
 fs
