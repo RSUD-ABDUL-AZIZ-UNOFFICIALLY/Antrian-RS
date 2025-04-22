8cf960455a9e6b30d6d4dbdf78c869b5d68b5a86
fakhryhizballah <falehry88@gmail.com>
2025-04-22 16:29:45 +0700

    epos netwrok

--- usb.js
+++ usb.js
@@ -1,17 +1,31 @@
+require('dotenv').config();
 const escpos = require('escpos');
+const e = require('express');
 escpos.USB = require('escpos-usb');
+escpos.Network = require('escpos-network');
 
 
 const cetakAntrian = function (nomor_antri) {
-
     let date = new Date();
     let date_now = date.getDate() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + date.getFullYear();
     let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
     try {
         const devices = escpos.USB.findPrinter();
         console.log(devices)
-        const options = { encoding: "GB18030" /* default */ }
-        const device = new escpos.USB();
+        let device = [];
+        if (devices.length === 0) {
+            console.log('Printer tidak ditemukan');
+            console.log(process.env.PRINTER_IP);
+            device = new escpos.Network(process.env.PRINTER_IP);
+
+        } else {
+            console.log('Printer ditemukan');
+            device = new escpos.USB();
+        }
+        // const options = { encoding: "GB18030" /* default */ }
+        const options = { encoding: "EUC-KR" /* default */ }
+        // const device = new escpos.USB();   
+        // console.log(device)
         const printer = new escpos.Printer(device, options);
         device.open(function () {
             printer
@@ -56,6 +70,7 @@ const cetakAntrian = function (nomor_antri) {
     return true;
 
 }
+cetakAntrian('A001');
 module.exports = {
     cetakAntrian
 }
